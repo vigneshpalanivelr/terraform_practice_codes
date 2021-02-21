7e9ae480c43e5abdc9ae85c9d349adf0778c2413
vigneshpalanivelr <vignesh.palanivelr@gmail.com>
2021-02-21 15:51:13 +0530

    Updated ASG

--- modules/all_modules/asg_module/auto_scaling_group/variables.tf
+++ modules/all_modules/asg_module/auto_scaling_group/variables.tf
@@ -4,5 +4,5 @@ variable "asg_min_size"               {}
 variable "asg_desired_capacity"       { default = 0   }
 # variable "asg_availability_zones"     { type = "list" }
 variable "asg_health_check_type"      {}
-variable "asg_tags"                   { type = list(map)  }
+variable "asg_tags"                   { type = list(string)  }
 variable "asg_force_delete"           {}
